565d5c95c3f8e99181b3256d4db57cd737e00fbd
Ian Yang <ianyangyi@gmail.com>
2014-03-05 23:38:32 -0800

    stuff working

--- app/assets/javascripts/controllers/create-ctrl.js
+++ app/assets/javascripts/controllers/create-ctrl.js
@@ -1,6 +1,23 @@
-Bundle.controller('CreateCtrl', ['$scope',
-    function($scope) {
+Bundle.controller('CreateCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
 
-      console.log("Create");
+        $scope.items = null;
+        $scope.appReady = false;
+        $scope.UUID = $location.path().split('/')[2];
+
+        var fetchItems = function() {
+            $http.get('/api/transactions/'+$scope.UUID).success(function(data){
+                $scope.appReady = true;
+                $scope.items = data;
+            }).error(function(data){
+                $location.path("/");
+            });
+        };
+
+        var init = function() {
+            fetchItems();
+        };
+
+        init();
 
 }]);
