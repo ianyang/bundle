ffb0c965d32b20787893ff2baa61646674bcdc84
Guillaume Delente <guillaume.delente+github@gmail.com>
2014-03-05 17:43:10 -0800

    Adds seller email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140306013422) do
+ActiveRecord::Schema.define(version: 20140306014238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140306013422) do
     t.integer  "discount_rate"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
