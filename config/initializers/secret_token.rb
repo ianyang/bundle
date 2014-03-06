4db68778d4bf68360362ec30efac2912db837d37
Ian Yang <ianyangyi@gmail.com>
2014-03-05 16:19:36 -0800

    removed secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bundle::Application.config.secret_key_base = '2f5cdf298f0a4c52530a3802824bc2193f63875d56b3e5457b51334ce7df2aa717bcd4525d99f4f83aa3fb7e14ba170340f4c983bee86808c77d9618d596190c'
