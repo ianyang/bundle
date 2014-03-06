db82efbe0b62e91fc57d3cd7f18b9e4d71218ec3
Ian Yang <ianyangyi@gmail.com>
2014-03-05 21:40:48 -0800

    home page done

--- app/assets/javascripts/controllers/home-ctrl.js
+++ app/assets/javascripts/controllers/home-ctrl.js
@@ -1,6 +1,27 @@
 Bundle.controller('HomeCtrl', ['$scope',
     function($scope) {
 
-      console.log("home");
+        // initializing scope variables
+        $scope.email = "";
+
+        // get started
+        $scope.proceed = function() {
+            console.log($scope.email);
+        };
+
+
+        // carousel
+
+        $scope.moveLeft = function() {
+
+
+        };
+
+        $scope.moveRight = function() {
+
+
+
+        };
+
 
 }]);
