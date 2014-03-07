33a0ee5e6f4f3299af19ef6e0b468fe7f219aaab
sbLL <skye@livelovely.com>
2014-03-07 08:58:16 -0800

    carousel boom

--- app/assets/javascripts/controllers/home-ctrl.js
+++ app/assets/javascripts/controllers/home-ctrl.js
@@ -4,6 +4,8 @@ Bundle.controller('HomeCtrl', ['$scope',
         // initializing scope variables
         $scope.email = "";
         $scope.inputError = false;
+        var currentTutorial = 1;
+
 
         // get started
         $scope.proceed = function() {
@@ -18,17 +20,57 @@ Bundle.controller('HomeCtrl', ['$scope',
 
 
         // carousel
+        var moveTutorial = function(direction) {
+        
+            if (direction=='right'){
+                if (currentTutorial==1){
+                    //alert(currentTutorial)
+                    $(card2).addClass("active");
+                    $(card1).removeClass("active");
+                    $(arrow1).removeClass("end");
+                     currentTutorial=2;
+                }
+
+                else if (currentTutorial==2){
+                    $(card3).addClass("active");
+                    $(card2).removeClass("active");
+                    $(arrow2).addClass("end");
+                    currentTutorial=3;
+
+                }
+
+                else if (currentTutorial==3){
+                 /*   alert("well that should not have happened");*/
+                }
+
+            } else if (direction=='left'){
+                if (currentTutorial==3){
+                    $(card2).addClass("active");
+                    $(card3).removeClass("active");
+                    $(arrow2).removeClass("end");
+                    currentTutorial= 2;
+                }
+
+                else if (currentTutorial==2){
+                    $(card1).addClass("active");
+                    $(card2).removeClass("active");
+                    $(arrow1).addClass("end");
+                    currentTutorial=1;
+
+                }
+
+                else if (currentTutorial==1){
+                   /* alert("nope, cant go left zoolander")*/
+                }
+            }
+        };
 
         $scope.moveLeft = function() {
-
-
+            moveTutorial('left');
         };
 
         $scope.moveRight = function() {
-
-
-
+            moveTutorial('right');
         };
-
 
 }]);
